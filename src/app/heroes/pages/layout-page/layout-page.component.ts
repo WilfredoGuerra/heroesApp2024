09fecb3827bec877ac3aa6c53405e28d6c10de33
Wilfredo Guerra <willigue@gmail.com>
2024-07-17 19:41:04 -0400

    primer commit

---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: ``
+})
+export class LayoutPageComponent {
+
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ]
+
+  constructor(
+    private authservice: AuthService,
+    private router: Router
+  ){}
+
+  get user(): User | undefined {
+    return this.authservice.currentUser;
+  }
+
+  onLogout(){
+    this.authservice.logout();
+    this.router.navigate(['/auth/login'])
+  }
+}
